f19f4e4aaaaf67a34b4a6ea1ce403c32f08aac70
Jeff Lee <jeff@healthiq.com>
2016-09-01 17:24:01 -0700

    Moved gecimi query to lib

--- index.js
+++ index.js
@@ -9,39 +9,17 @@
  */
 
 var program = require('commander');
-var chineseConv = require('chinese-conv');
-var qs = require('querystring');
-var Promise = require('bluebird');
-var request = Promise.promisifyAll(require('request'), { multiArgs: true });
 
-var LYRICS_API_URL = 'http://gecimi.com/api/lyric/';
+var APIs = {
+	gecimi: require('./lib/gecimi'),
+	wangyiyun: require('./lib/wangyiyun')
+};
 
 program
 	.version('v0.1.0')
 	.arguments('<name> [artist]')
 	.action((name, artist) => {
-		var songNameSimplified = qs.escape(chineseConv.sify(name));
-
-		if (artist) {
-			songNameSimplified += '/' + qs.escape(chineseConv.sify(artist));
-		}
-
-    	request
-			.getAsync(LYRICS_API_URL + songNameSimplified, { json: true })
-			.spread((response, body) => {
-				if (body.result.length > 0) {
-					return body.result[0].lrc;
-				} else {
-					throw new Error('Song not found');
-				}
-			})
-			.then(request.getAsync)
-			.spread((response, body) => {
-				console.log(body);
-			})
-			.catch((e) => {
-				console.log('Song not found');
-			});
+		APIs.gecimi.lyrics(name, artist);
 	});
 
 program.parse(process.argv);
